e1d91af3e03d07b50f8470d940d5b009c167e94f
khisus19 <khisus19@gmail.com>
2023-02-13 11:04:45 -0600

    Counter app done - sect 4 finnished

--- 03-couter-app-vite/src/FirstApp.jsx
+++ 03-couter-app-vite/src/FirstApp.jsx
@@ -1,21 +1,28 @@
+import { useState } from 'react';
 import PropTypes from "prop-types";
 
-export const FirstApp = ({ title, subtitle }) => {
 
-  // console.log();
+export const FirstApp = ({ value }) => {
+  
+  const [ counter, setCounter ] = useState(value);
 
+  const handleAdd = () => setCounter( counter + 1);
+  const handleSub = () => setCounter( counter - 1);
+  const handleReset = () => setCounter( value );
 
   return (
     <>
-      <h1>{ title }</h1>
-      <p>{ subtitle }</p>
+      <h1>Counter App</h1>
+      <p>{ counter }</p>
+      <button onClick={ handleAdd } >+1</button>
+      <button onClick={ handleSub } >-1</button>
+      <button onClick={ handleReset } >Reset</button>
     </>
   )
 }
 
 FirstApp.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string,
+  value: PropTypes.number,
 }
 
 FirstApp.defaultProps = {
--- 03-couter-app-vite/src/main.jsx
+++ 03-couter-app-vite/src/main.jsx
@@ -8,7 +8,7 @@ import './styles.css'
 ReactDOM.createRoot( document.getElementById("root")).render(
   <React.StrictMode>
 
-    <FirstApp title={ "Soy string" } />
+    <FirstApp value={ 1 } />
     
   </React.StrictMode>
 )
