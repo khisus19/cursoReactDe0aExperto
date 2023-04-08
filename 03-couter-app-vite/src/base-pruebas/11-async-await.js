f877ca93638f711737e8aafc01512616eba5f609
khisus19 <khisus19@DESKTOP-JN9A6AK>
2023-04-08 12:11:04 -0600

    Practicando tests en JS async

--- 03-couter-app-vite/src/base-pruebas/11-async-await.js
+++ 03-couter-app-vite/src/base-pruebas/11-async-await.js
@@ -5,7 +5,7 @@ export const getImagen = async() => {
 
     try {
 
-        const apiKey = '9hNhYfR5jnRYPkOi33k6MxzUwtPNqJF';
+        const apiKey = '9hNhYfR5jnRYPkOi33k6MxzUwtPNqJFt';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
--- 03-couter-app-vite/test/base-pruebas/11-async-await.test.js
+++ 03-couter-app-vite/test/base-pruebas/11-async-await.test.js
@@ -3,12 +3,21 @@ import { getImagen } from "../../src/base-pruebas/11-async-await"
 
 describe('Pruebas en 11-async-await.js', () => { 
     
-    test('Debe retornar un url', async () => { 
+    test('Debe retornar un url de giphy', async () => { 
         
         const resp = await getImagen();
         // console.log(url)
-        expect(resp).toBe("No se encontro la imagen");
+        expect( resp.startsWith("https:") ).toBeTruthy();
+
     })
+
+    // test('Debe retornar un error si no tenemos un api key', async () => { 
+        
+    //     const resp = await getImagen();
+    //     // console.log(url)
+    //     expect( resp ).toBe("No se encontro la imagen");
+
+    // })
 })
 
 
