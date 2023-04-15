c9089e52c71799be752033a2c1a82050f25b77eb
khisus19 <khisus19@DESKTOP-JN9A6AK>
2023-04-15 17:16:18 -0600

    class 77

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState([ "One punch", "Dragon ball" ]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, "Inuyasha"])
+  }
+
   return (
     <>
+      {/* titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory />
+
+      {/* Listado de gif */}
+      <button onClick={ onAddCategory }>Agregar Categoria</button>
+      <ol>
+        { categories.map(category => {
+          return <li key={category}>{ category }</li>
+        }) }
+      </ol>
+        {/* Gif Item*/}
+
+
+
     </>
   )
 }
