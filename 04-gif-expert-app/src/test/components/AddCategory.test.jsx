5533b08592fe7320bc78be6a3dc6568ab8e6eef4
khisus19 <khisus19@gmail.com>
2023-04-24 10:58:54 -0600

    Test AddCategory component

--- 04-gif-expert-app/src/test/components/AddCategory.test.jsx
+++ 04-gif-expert-app/src/test/components/AddCategory.test.jsx
@@ -13,4 +13,38 @@ describe('Pruebas en AddCategory', () => {
     expect( input.value ).toBe('Saitama');
 
   });
+
+  test('Debe de llamar onNewCategory si el input tiene un valor', () => { 
+    
+    const inputValue = 'Saitama';
+    const onNewCategory = jest.fn();
+
+    render( <AddCategory onNewCategory={ onNewCategory } /> );
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input( input, { target: { value: inputValue } } );
+    fireEvent.submit( form ); 
+    // screen.debug();
+    expect( input.value ).toBe('');
+
+    expect( onNewCategory ).toHaveBeenCalled();
+    expect( onNewCategory ).toHaveBeenCalledTimes(1);
+    expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+
+  })
+
+  test('Debe llamar el onNewCategory si el input esta vacio', () => { 
+    
+    const onNewCategory = jest.fn();
+
+    render( <AddCategory onNewCategory={ onNewCategory } /> );
+
+    const form = screen.getByRole('form');
+    fireEvent.submit( form ); 
+
+    expect( onNewCategory ).not.toHaveBeenCalled();
+
+  })
 });
