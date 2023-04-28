17d1b1a6a2c729b6de80aabec7a96aa9d4bc9ca8
khisus19 <khisus19@gmail.com>
2023-04-28 00:14:34 -0600

    Tests for GifGrid component

--- 04-gif-expert-app/src/test/components/GifGrid.test.jsx
+++ 04-gif-expert-app/src/test/components/GifGrid.test.jsx
@@ -1,22 +1,47 @@
 import { render, screen } from "@testing-library/react"
 import { GifGrid } from "../../components/GifGrid"
+import { useFetchGifs } from "../../hooks/useFetchGifs";
 
+jest.mock("../../hooks/useFetchGifs");
 
 describe('Pruebas en <GifGrid />', () => { 
 
   const category = "Saitama"
 
-  test('Debe mostrar el loading inicialmente', () => { 
+  test('Debe mostrar el loading inicialmente', () => {
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
     
     render( <GifGrid category={ category } />);
     expect( screen.getByText( "Cargando..." ) );
     expect( screen.getByText( category ) );
   });
 
-  test('Debe mostrar items cuando se cargan las imagenes useFetchGifs', () => { 
+  test('Debe mostrar items cuando se cargan las imagenes useFetchGifs', () => {
 
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "https://localhost/saitama.jpg"
+      },
+      {
+        id: "123",
+        title: "Goku",
+        url: "https://localhost/goku.jpg"
+      }
+    ];
 
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
 
+    render( <GifGrid category={ category } />);
+    expect( screen.getAllByRole("img").length ).toBe(2);
 
   });
 
