d641039aa4af21c5b40a2d45099302f20ba7b478
khisus19 <khisus19@gmail.com>
2023-08-04 11:33:00 -0600

    Practica del useLayoutEffect 1

--- 05-hook-app/src/03-examples/CharacterDisplay.jsx
+++ 05-hook-app/src/03-examples/CharacterDisplay.jsx
@@ -2,7 +2,10 @@
 
 export const CharacterDisplay = ({ name, species, image}) => {
   return (
-    <blockquote className="blockquote text-end">
+    <blockquote 
+      className="blockquote text-end"
+      style={{ display: "flex"}}
+    >
       <p className="mb-1">{ name }</p>
       <footer className="blockquote-footer">{ species }</footer>
       <img src={ image } />
---
+++ 05-hook-app/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,35 @@
+import { useCounter, useFetch } from "../hooks";
+import { CharacterDisplay, CharacterLoading } from "../03-examples";
+
+const API = "https://rickandmortyapi.com/api/character/";
+
+export const Layout = () => {
+  
+  const { counter, increment, decrement } = useCounter(1);
+
+  const id = ( counter <= 0 ) ? 1 : counter
+  
+  const { data, isLoading, hasError } = useFetch(`${API}${id}`);
+  const { name, species, image } = !!data && data;
+
+  return (
+    <>
+      <h1 className="mt-2">Rick and Morty characters</h1>
+
+      {isLoading ? <CharacterLoading /> : <CharacterDisplay name={ name } species={ species } image={ image } />}
+
+      <button 
+        className="btn btn-primary"
+        onClick={() => increment()}
+      >Change character</button>
+      
+
+      <button 
+        className="btn btn-primary mt-2"
+        disabled={ isLoading || id <= 1 }
+        onClick={() => decrement()}
+      >Return to previous character</button>
+      
+    </>
+  )
+}
