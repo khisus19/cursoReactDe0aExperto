abd16a647f25a16b94dff862b4200104b49a05de
khisus19 <khisus19@gmail.com>
2023-08-06 23:09:13 -0600

    Hook useMemo e inicio de useCallback

---
+++ 05-hook-app/src/06-memos/CallbackHook.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import { ShowIncrement } from "./ShowIncrement";
+
+
+
+export const CallbackHook = () => {
+
+  const [counter, setcounter] = useState( 10 );
+
+  const incrementFather = () => {
+    setcounter( counter + 1);
+  }
+
+  return (
+    <>
+      <h1>useCallback Hook: { counter }</h1>
+      <hr />
+
+      <ShowIncrement increment={ incrementFather } />
+
+    </>
+  )
+}
