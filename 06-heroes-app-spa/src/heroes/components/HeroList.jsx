63d74b4820f685212ce5704d5b8adb8c70296c69
khisus19 <khisus19@gmail.com>
2023-08-31 19:48:35 -0600

    Building HeroList, HeroCard and getHeroByPublisher

---
+++ 06-heroes-app-spa/src/heroes/components/HeroList.jsx
@@ -0,0 +1,21 @@
+import { HeroCard } from "./";
+import { getHeroesByPublisher } from "../helpers";
+
+
+export const HeroList = ({ publisher }) => {
+
+  const heroes = getHeroesByPublisher( publisher );
+
+  return (
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      { 
+        heroes.map( heroe => (
+          <HeroCard 
+            key={ heroe.id }
+            { ...heroe }
+          />
+        ))
+      }
+    </div>
+  )
+};
