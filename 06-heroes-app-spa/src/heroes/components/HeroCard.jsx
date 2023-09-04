523885550bc3a592e8a72b12e071a7f4fd060927
khisus19 <khisus19@gmail.com>
2023-09-03 22:54:42 -0600

    Search functionality done - sect 14 end

--- 06-heroes-app-spa/src/heroes/components/HeroCard.jsx
+++ 06-heroes-app-spa/src/heroes/components/HeroCard.jsx
@@ -17,11 +17,11 @@ export const HeroCard = ({ id,  superhero,  publisher,  alter_ego,  first_appear
       <div className="card">
 
         <div className="row no-gutters">
-          <div className="col-4">
+          <div className="col-5">
             <img src={ heroImageUrl } className="card-img" alt={ superhero } />
           </div>
 
-          <div className="col-8">
+          <div className="col-7">
 
             <div className="card-body">
               <h5 className="card-title">{ superhero }</h5>
