1baf9dba511489e045c43f789492ce2705a9a561
khisus19 <khisus19@gmail.com>
2023-09-05 16:23:53 -0600

    Add login functionality

--- 06-heroes-app-spa/src/ui/components/NavBar.jsx
+++ 06-heroes-app-spa/src/ui/components/NavBar.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../auth";
 
 
 export const Navbar = () => {
 
     const navigate = useNavigate();
+    const { user } = useContext( AuthContext );
     
     const onLogout = () => {
         navigate("/login", {
@@ -50,7 +53,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                   <span className="nav-item nav-link text-pimary">
-                    Jesús
+                    { user?.name }
                   </span>
 
                   <button
