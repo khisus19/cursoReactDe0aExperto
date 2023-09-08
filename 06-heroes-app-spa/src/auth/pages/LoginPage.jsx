5c521ee917ae356e5bcd41ab7dff7494e7905327
khisus19 <khisus19@gmail.com>
2023-09-07 22:16:46 -0600

    Add public route and last path visited persistancy

--- 06-heroes-app-spa/src/auth/pages/LoginPage.jsx
+++ 06-heroes-app-spa/src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem("lastPath") || "/";
+
     login( "Jesus Alvarez" );
 
-    navigate("/", {
+    navigate( lastPath, {
       replace: true
     });
   };
