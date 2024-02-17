895cbcb5e9b718be478070def810bf18b4050060
khisus19 <khisus19@gmail.com>
2024-02-17 13:23:02 -0600

    Create NavBar and SideBar

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,13 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ratione?</Typography>
+
+      {/* NothingSelected */}
+      {/* Noteview */}
+    </JournalLayout>
   )
 }
