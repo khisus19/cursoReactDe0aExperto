f8a806e2c30b518fa0062c04bccc3ed96c201450
khisus19 <khisus19@gmail.com>
2024-03-01 11:42:43 -0600

    RTK query

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
+import { useGetTodoByIdQuery, useGetTodosQuery } from "./store/apis/todosApi"
 
 export const TodoApp = () => {
+
+  const [ todoId, settodoId ] = useState(1);
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoByIdQuery(todoId);
+
+  const nextTodo = () => {
+    settodoId( todoId + 1 );
+  }
+
+  const prevTodo = () => {
+    if ( todoId === 1) return;
+    settodoId( todoId - 1);
+  }
+
   return (
     <>
       <h1>Todos - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading { isLoading ? "True": "False" }</h4>
 
-      <pre>...</pre>
+      <pre>{ JSON.stringify( todo ) }</pre>
 
-      <button>
+      <button onClick={ prevTodo }>
+        Previous todo
+      </button>
+      <button onClick={ nextTodo }>
         Next todo
       </button>
+
+      {/* <ul>
+        { todos.map( todo => (
+          <li key={ todo.id }>
+            <strong> { todo.completed ? "Done" : "Pending" } </strong>
+            { todo.title }
+          </li>
+        ) ) }
+      </ul> */}
+
     </>
   )
 }
